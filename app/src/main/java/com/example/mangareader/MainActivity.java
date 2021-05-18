5deabcb3c58127dfe1154558748baa41b08b984d
Manuelo <57776467+ManueloTanjay@users.noreply.github.com>
2021-05-18 17:51:07 -0400

    homepage layout

--- app/src/main/java/com/example/mangareader/MainActivity.java
+++ app/src/main/java/com/example/mangareader/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public String baseURL = "https://api.mangadex.org";
+    public String searchManga = "/manga?title=%s&limit=100&contentRating%5B%5D=safe";
+    public String getChapters = "/chapter?manga=%s&limit=100&offset=%s&translatedLanguage=en";
+    public String getBaseURL = "/at-home/server/%s";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
