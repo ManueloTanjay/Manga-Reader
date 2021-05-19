1449e817bc5f397f03794d15425285262db663dc
Manuelo <57776467+ManueloTanjay@users.noreply.github.com>
2021-05-19 00:37:18 -0400

    fragment stack now goes to homepage

--- app/src/main/java/com/example/mangareader/MainActivity.java
+++ app/src/main/java/com/example/mangareader/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.mangareader;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.widget.SearchView;
 
 public class MainActivity extends FragmentActivity {
+
+    SearchView searchView;
 
     public String baseURL = "https://api.mangadex.org";
     public String searchManga = "/manga?title=%s&limit=100&contentRating%5B%5D=safe";
@@ -18,11 +23,36 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SearchResultsFragment results = new SearchResultsFragment();
+        searchView = (SearchView) findViewById(R.id.searchView);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.homepage, results);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        SearchView.OnQueryTextListener queryTextListener
+                = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                FragmentManager fm = getSupportFragmentManager();
+                for(int i = 0; i < fm.getBackStackEntryCount(); ++i) {
+                    fm.popBackStack();
+                }
+
+                SearchResultsFragment results = new SearchResultsFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.homepage, results);
+                transaction.addToBackStack(null);
+                transaction.commit();
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        };
+
+        searchView.setOnQueryTextListener(queryTextListener);
     }
+
+
 }
