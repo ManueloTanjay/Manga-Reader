a967d26cd01c2f277f44432fc75213bba6b9dc6e
Thomas <suncharn.pipithkul@gmail.com>
2021-05-21 14:41:56 -0400

    idk

--- app/src/main/java/com/example/mangareader/pages/PageActivity.java
+++ app/src/main/java/com/example/mangareader/pages/PageActivity.java
@@ -16,6 +16,13 @@ public class PageActivity extends AppCompatActivity {
     TextView testData;
     String id;
     ArrayList<Bitmap> images = new ArrayList<>();
+    ArrayList<String> imgsUrl = new ArrayList<String>() {
+        {
+            add("https://ner5efrtww4pr.98s0casa9jrd0.mangadex.network:443/8SbpeQd2KxLt2hfMSFFRtCZXxnrpqZoenNekLzqOsW6SRMlYpTo-i9fgxcFXM13q0G-RPf5EgltkQ9l_h9-5n5nYce5NWN69XfB8XPiwxDhpHI_pbm-Hddn5C9GKNa9XTWFUVDgEgmMbaueU9Lg_PNNC8jgtHU6JBQAiaJS3ubLIbVHVRGIhsL2Lzk28Lzom/data/040ba5c0bc64fe338e0580a728b9f31e/21-f7e03fca6ea8676446ee3a0267205fb8fcce272d37415fffebc34ccb4163183d.png");
+            add("https://ner5efrtww4pr.98s0casa9jrd0.mangadex.network:443/8SbpeQd2KxLt2hfMSFFRtCZXxnrpqZoenNekLzqOsW6SRMlYpTo-i9fgxcFXM13q0G-RPf5EgltkQ9l_h9-5n5nYce5NWN69XfB8XPiwxDhpHI_pbm-Hddn5C9GKNa9XTWFUVDgEgmMbaueU9Lg_PNNC8jgtHU6JBQAiaJS3ubLIbVHVRGIhsL2Lzk28Lzom/data/040ba5c0bc64fe338e0580a728b9f31e/22-10c1e42b46ef33a2ce2229a02c17715b11e75f3af823ddbc915b3a9dc272a00f.png");
+            add("https://ner5efrtww4pr.98s0casa9jrd0.mangadex.network:443/8SbpeQd2KxLt2hfMSFFRtCZXxnrpqZoenNekLzqOsW6SRMlYpTo-i9fgxcFXM13q0G-RPf5EgltkQ9l_h9-5n5nYce5NWN69XfB8XPiwxDhpHI_pbm-Hddn5C9GKNa9XTWFUVDgEgmMbaueU9Lg_PNNC8jgtHU6JBQAiaJS3ubLIbVHVRGIhsL2Lzk28Lzom/data/040ba5c0bc64fe338e0580a728b9f31e/23-3bf0cecdc6241b1b50cf64415c43af9cfa3f6c07b1356615efdb428b065ba1ba.png");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +33,8 @@ public class PageActivity extends AppCompatActivity {
 
         PageMangaAdapter adapter = new PageMangaAdapter(this);
         viewPager.setAdapter(adapter);
+
 //        testData = findViewById(R.id.testInfo);
-//
 //        getData();
     }
 
--- app/src/main/java/com/example/mangareader/pages/PageMangaAdapter.java
+++ app/src/main/java/com/example/mangareader/pages/PageMangaAdapter.java
@@ -10,10 +10,19 @@ import androidx.viewpager.widget.PagerAdapter;
 
 import com.example.mangareader.R;
 
+import java.util.ArrayList;
+
 public class PageMangaAdapter extends PagerAdapter {
 
     private Context ctx;
     private int[] imageArray = new int[] {R.drawable.img1, R.drawable.img2, R.drawable.img3};
+    private ArrayList<String> imgURLArray = new ArrayList<String>() {
+        {
+            add("https://ner5efrtww4pr.98s0casa9jrd0.mangadex.network:443/8SbpeQd2KxLt2hfMSFFRtCZXxnrpqZoenNekLzqOsW6SRMlYpTo-i9fgxcFXM13q0G-RPf5EgltkQ9l_h9-5n5nYce5NWN69XfB8XPiwxDhpHI_pbm-Hddn5C9GKNa9XTWFUVDgEgmMbaueU9Lg_PNNC8jgtHU6JBQAiaJS3ubLIbVHVRGIhsL2Lzk28Lzom/data/040ba5c0bc64fe338e0580a728b9f31e/21-f7e03fca6ea8676446ee3a0267205fb8fcce272d37415fffebc34ccb4163183d.png");
+            add("https://ner5efrtww4pr.98s0casa9jrd0.mangadex.network:443/8SbpeQd2KxLt2hfMSFFRtCZXxnrpqZoenNekLzqOsW6SRMlYpTo-i9fgxcFXM13q0G-RPf5EgltkQ9l_h9-5n5nYce5NWN69XfB8XPiwxDhpHI_pbm-Hddn5C9GKNa9XTWFUVDgEgmMbaueU9Lg_PNNC8jgtHU6JBQAiaJS3ubLIbVHVRGIhsL2Lzk28Lzom/data/040ba5c0bc64fe338e0580a728b9f31e/22-10c1e42b46ef33a2ce2229a02c17715b11e75f3af823ddbc915b3a9dc272a00f.png");
+            add("https://ner5efrtww4pr.98s0casa9jrd0.mangadex.network:443/8SbpeQd2KxLt2hfMSFFRtCZXxnrpqZoenNekLzqOsW6SRMlYpTo-i9fgxcFXM13q0G-RPf5EgltkQ9l_h9-5n5nYce5NWN69XfB8XPiwxDhpHI_pbm-Hddn5C9GKNa9XTWFUVDgEgmMbaueU9Lg_PNNC8jgtHU6JBQAiaJS3ubLIbVHVRGIhsL2Lzk28Lzom/data/040ba5c0bc64fe338e0580a728b9f31e/23-3bf0cecdc6241b1b50cf64415c43af9cfa3f6c07b1356615efdb428b065ba1ba.png");
+        }
+    };
 
     public PageMangaAdapter(Context context) {
         ctx = context;
@@ -21,7 +30,9 @@ public class PageMangaAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return imageArray.length;
+        if (imgURLArray != null)
+            return imgURLArray.size();
+        return 0;
     }
 
     @Override
@@ -34,7 +45,8 @@ public class PageMangaAdapter extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         ImageView imgView = new ImageView(ctx);
         imgView.setScaleType(ImageView.ScaleType.CENTER_CROP); // center the imgView
-        imgView.setImageResource(imageArray[position]); // set imgView to hold the image
+//        imgView.setImageResource(imageArray[position]); // set imgView to hold the image
+
         container.addView(imgView, 0); // set container to hold imgView
         return imgView;
     }
